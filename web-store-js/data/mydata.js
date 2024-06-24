678611befa99df67e288ffc4475bf9d2a7ba9543
kserb04 <klara.serbetar@gmail.com>
2024-06-25 01:23:57 +0200

    add data for js site

---
+++ web-store-js/data/mydata.js
@@ -0,0 +1,319 @@
+const data = {
+  website: "Food",
+  categories: [
+    {
+      name: "Fruits",
+      image: ".jpg",
+      products: [
+        {
+          name: "Apple",
+          image: "../images/apple.jpg",
+          id: 1,
+        },
+        {
+          name: "Pear",
+          image: "../images/pear.jpg",
+          id: 2,
+        },
+        {
+          name: "Grapes",
+          image: "../images/grapes.jpg",
+          id: 3,
+        },
+        {
+          name: "Banana",
+          image: "../images/banana.jpg",
+          id: 4,
+        },
+        {
+          name: "Orange",
+          image: "../images/orange.jpg",
+          id: 5,
+        },
+      ],
+    },
+    {
+      name: "Vegetables",
+      image: ".jpg",
+      products: [
+        {
+          name: "Lettuce",
+          image: "../images/salad.jpg",
+          id: 6,
+        },
+        {
+          name: "Tomato",
+          image: "../images/tomato.jpg",
+          id: 7,
+        },
+        {
+          name: "Krumpir",
+          image: "../images/potato.jpg",
+          id: 8,
+        },
+        {
+          name: "Luk",
+          image: "../images/onion.jpg",
+          id: 9,
+        },
+        {
+          name: "Paprika",
+          image: "../images/pepper.jpg",
+          id: 10,
+        },
+      ],
+    },
+    {
+      name: "Dairy products",
+      image: ".jpg",
+      products: [
+        {
+          name: "Milk",
+          image: "../images/milk.jpg",
+          id: 16,
+        },
+        {
+          name: "Cream cheese",
+          image: "../images/creamcheese.jpg",
+          id: 17,
+        },
+        {
+          name: "Yoghurt",
+          image: "../images/yoghurt.jpg",
+          id: 18,
+        },
+        {
+          name: "Cottage cheese",
+          image: "../images/cheese2.jpg",
+          id: 19,
+        },
+        {
+          name: "Hard cheese",
+          image: "../images/cheese.jpg",
+          id: 20,
+        },
+      ],
+    },
+    {
+      name: "Bread & pastries",
+      image: ".jpg",
+      products: [
+        {
+          name: "Cornbread",
+          image: "../images/bread2.jpg",
+          id: 26,
+        },
+        {
+          name: "Bread with seeds",
+          image: "../images/bread1.jpg",
+          id: 27,
+        },
+        {
+          name: "Bun",
+          image: "../images/zemlja.jpg",
+          id: 28,
+        },
+        {
+          name: "Bagel",
+          image: "../images/bagel.jpg",
+          id: 29,
+        },
+        {
+          name: "Croissant",
+          image: "../images/croissant.jpg",
+          id: 30,
+        },
+      ],
+    },
+    {
+      name: "Rice, pasta & grains",
+      image: ".jpg",
+      products: [
+        {
+          name: "Rice",
+          image: "../images/rice.jpg",
+          id: 31,
+        },
+        {
+          name: "Spaghetti",
+          image: "../images/pasta2.jpg",
+          id: 32,
+        },
+        {
+          name: "Pasta",
+          image: "../images/pasta1.jpg",
+          id: 33,
+        },
+        {
+          name: "Oats",
+          image: "../images/oats.jpg",
+          id: 34,
+        },
+        {
+          name: "Millet",
+          image: "../images/millet.jpg",
+          id: 35,
+        },
+      ],
+    },
+    {
+      name: "Kitchen pantry",
+      image: ".jpg",
+      products: [
+        {
+          name: "Salt",
+          image: "../images/salt.jpg",
+          id: 36,
+        },
+        {
+          name: "Black pepper",
+          image: "../images/pepper2.jpg",
+          id: 37,
+        },
+        {
+          name: "Sugar",
+          image: "../images/sugar.jpg",
+          id: 38,
+        },
+        {
+          name: "Tomato suace",
+          image: "../images/tomatoes.jpg",
+          id: 39,
+        },
+        {
+          name: "Cinnamon",
+          image: "../images/cinnamon.jpg",
+          id: 40,
+        },
+      ],
+    },
+    {
+      name: "Sweets",
+      image: ".jpg",
+      products: [
+        {
+          name: "Sour candies",
+          image: "../images/sourgb.jpg",
+          id: 21,
+        },
+        {
+          name: "Caramel candy",
+          image: "../images/caramel.jpg",
+          id: 22,
+        },
+        {
+          name: "Chocolate",
+          image: "../images/chocolate.jpg",
+          id: 23,
+        },
+        {
+          name: "Marshmallow",
+          image: "../images/marshmallow.jpg",
+          id: 24,
+        },
+        {
+          name: "Gummy bears",
+          image: "../images/gummybears.jpg",
+          id: 25,
+        },
+      ],
+    },
+    {
+      name: "Cereals",
+      image: ".jpg",
+      products: [
+        {
+          name: "Choco balls",
+          image: "../images/chocoballs.jpg",
+          id: 41,
+        },
+        {
+          name: "Mixed",
+          image: "../images/cereal.jpg",
+          id: 42,
+        },
+        {
+          name: "Chocolate",
+          image: "../images/choco.jpg",
+          id: 43,
+        },
+        {
+          name: "Fruit loops",
+          image: "../images/fruitloops.jpg",
+          id: 44,
+        },
+        {
+          name: "Cornflakes",
+          image: "../images/cornflakes.jpg",
+          id: 45,
+        },
+      ],
+    },
+    {
+      name: "Cakes",
+      image: ".jpg",
+      products: [
+        {
+          name: "Macarons",
+          image: "../images/macarons.jpg",
+          id: 11,
+        },
+
+        {
+          name: "Cupcakes",
+          image: "../images/cupcakes.jpg",
+          id: 12,
+        },
+        {
+          name: "Cake",
+          image: "../images/cake.jpg",
+          id: 13,
+        },
+        {
+          name: "Carrot cake",
+          image: "../images/carrotcake.jpg",
+          id: 14,
+        },
+
+        {
+          name: "Chocolate cake",
+          image: "../images/chococake.jpg",
+          id: 15,
+        },
+      ],
+    },
+    {
+      name: "Frozen foods",
+      image: ".jpg",
+      products: [
+        {
+          name: "Popsicle",
+          image: "../images/icecream.jpg",
+          id: 46,
+        },
+        {
+          name: "Vanilla ice-cream",
+          image: "../images/icecream2.jpg",
+          id: 47,
+        },
+        {
+          name: "Blueberries",
+          image: "../images/blueberries.jpg",
+          id: 48,
+        },
+        {
+          name: "Peas",
+          image: "../images/peas.jpg",
+          id: 49,
+        },
+        {
+          name: "French fries",
+          image: "../images/frozenpommes.jpg",
+          id: 50,
+        },
+      ],
+    },
+  ],
+};
+
+module.exports = data;
